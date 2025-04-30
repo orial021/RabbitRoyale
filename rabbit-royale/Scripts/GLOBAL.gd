50b28c6dc44a344c5b7f5d6e6cff1f7e9ecdec78
Manuel Barrera <manuelbarrerar98@gmail.com>
2025-04-29 21:11:31 -0400

    feat: menu agregado

---
+++ rabbit-royale/Scripts/GLOBAL.gd
@@ -0,0 +1,14 @@
+extends Node
+
+var axis : Vector2
+var can_use_heart : bool = true
+var can_use_bomb : bool = true
+var can_use_shield : bool = true
+var lives : int = 5
+var time : int = -1
+var sheep_dead : bool = true
+
+func get_axis() -> Vector2:
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	axis.x = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_pressed("ui_down"))
+	return axis.normalized()
