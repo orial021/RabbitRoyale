14119f1bcf2b9c5929f9343e35c795755789325a
Manuel Barrera <manuelbarrerar98@gmail.com>
2025-05-01 21:12:20 -0400

    Nuevas cosas

---
+++ rabbit-royale/Scenes/Menu/character_menu.gd
@@ -0,0 +1,19 @@
+extends CharacterBody3D
+class_name CharacterMenu
+
+var can_move : bool = false
+var rot : Vector3
+var angle : float
+const SPEED = 6
+const GRAVITY = 2
+const JUMP = 26
+
+func _process(delta: float) -> void:
+	if can_move:
+		motion_ctrl()
+		
+func motion_ctrl() -> void:
+	"MOVIMIENTO"
+	velocity.y -= GRAVITY
+	velocity.x = Global.get_axis().x * SPEED
+	velocity.z = Global.get_axis().y * -SPEED
--- rabbit-royale/Scenes/Menu/laporta.gd
+++ rabbit-royale/Scenes/Menu/laporta.gd
@@ -1,1 +1,11 @@
 extends Node3D
+
+@export var label : String
+@export var open_degree : float = 1.7
+
+func _ready() -> void:
+	$Label3D.text = label
+	
+func open() -> void:
+	var tween : Tween = create_tween().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property($Door, "rotation", Vector3(0,1.7, 0), 1.0)
