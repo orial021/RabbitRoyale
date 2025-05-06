b0db0c0423659f8bc808c581be5cc4460cde4ec1
Santiago Garcia <santiagogarciadb@gmail.com>
2025-05-06 19:41:29 -0400

    feat: menu updated

--- rabitRoyale/nuevo-proyecto-de-juego/Scripts/ScriptsMenu/character_menu.gd
+++ rabitRoyale/nuevo-proyecto-de-juego/Scripts/ScriptsMenu/character_menu.gd
@@ -12,8 +12,28 @@ func _process(delta: float) -> void:
 	if can_move: 
 		motion_ctrl()
 		
+	move_and_slide()
+		
 func motion_ctrl() -> void:
 	'''MOVIMIENTO'''
 	velocity.y -= GRAVITY
 	velocity.x = Global.get_axis().x * SPEED
-	velocity.y = Global.get_axis().y * SPEED
+	velocity.z = Global.get_axis().y * -SPEED
+	
+	'''ANIMACIONES'''
+	match is_on_floor():
+		true:
+			if Global.get_axis() != Vector2.ZERO:
+				$AnimationPlayer.play("Walk")
+			else:
+				$AnimationPlayer.play("Idle")
+	'''ANIMACIONES'''
+	if Global.get_axis() != Vector2.ZERO:
+		angle = atan2(Global.get_axis().x, -Global.get_axis().y)
+		rot = get_rotation()
+		rot.y = angle
+		set_rotation(rot)
+		
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Wave":
+		can_move = true
--- rabitRoyale/nuevo-proyecto-de-juego/Scripts/ScriptsMenu/menu.gd
+++ rabitRoyale/nuevo-proyecto-de-juego/Scripts/ScriptsMenu/menu.gd
@@ -2,3 +2,22 @@ extends Node3D
 @onready var door_create : Node3D = $"Settings/Crear Partida"
 @onready var door_join : Node3D = $Settings/Unirse
 var enter_game : bool = false
+
+func _ready() -> void:
+	ResourceLoader.load_threaded_request("res://Scenes/level.tscn")
+
+func _process(delta: float) -> void:
+	if not enter_game:
+		pass
+	else:
+		var scene_load_status = ResourceLoader.load_threaded_get_status("res://Scenes/level.tscn")
+		if scene_load_status == ResourceLoader.THREAD_LOAD_FAILED:
+			get_tree().call_deferred("change_scene_to_packed", ResourceLoader.load_threaded_get("res://Scenes/level.tscn"))
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
+
+
+func _on_area_3d_2_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
