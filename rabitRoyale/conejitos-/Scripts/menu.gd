c7c0416cf5fc19f85cf8064ea85e2c0838af08e4
hayli <haylimartinez2022@gmail.com>
2025-05-06 18:42:34 -0500

    feat: menu updated

--- rabitRoyale/conejitos-/Scripts/menu.gd
+++ rabitRoyale/conejitos-/Scripts/menu.gd
@@ -3,3 +3,19 @@ extends Node3D
 @onready var door_create: Node3D = $settings/Doorcreate
 @onready var door_join: Node3D = $settings/Doorjoin
 var enterGame : bool = false
+
+func _ready() -> void:
+	ResourceLoader.load_threaded_request("res://scenes/level.tscn")
+
+func _process(delta: float) -> void:
+	if not enterGame:
+		pass
+	else:
+		var scene_load_status = ResourceLoader.load_threaded_get_status("res://scenes/level.tscn")
+		if scene_load_status == ResourceLoader.THREAD_LOAD_FAILED:
+			get_tree().call_deferred("change_scene_to_packed", ResourceLoader.load_threaded_get("res://scenes/level.tscn"))
+			
+			
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
--- rabitRoyale/conejitos-/Scripts/menu/character_menu.gd
+++ rabitRoyale/conejitos-/Scripts/menu/character_menu.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 class_name CharacterMenu
 
-var can_move : bool = false
+var can_move : bool = true
 var rot: Vector3
 var angle : float 
 const SPEED = 6
@@ -11,8 +11,27 @@ const JUMP = 26
 func _process(delta: float) -> void:
 	if can_move:
 		motion_ctrl()
+	move_and_slide()
 		
 func motion_ctrl() -> void:
 	velocity.y -= GRAVITY
 	velocity.x = Global.get_axis().x * SPEED
 	velocity.z = Global.get_axis().y * - SPEED
+
+	match is_on_floor():
+		true: 
+			if Global.get_axis() != Vector2.ZERO:
+				$AnimationPlayer.play("Walk")
+			else:
+				$AnimationPlayer.play("Idle")
+				
+				
+	if Global.get_axis() != Vector2.ZERO:
+		angle = atan2(Global.get_axis().x , -Global.get_axis().y)
+		rot =get_rotation()
+		rot.y = angle
+		set_rotation(rot)
+		
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "wave":
+		can_move = true
