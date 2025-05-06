9d1a465fd1627202ea12e1c6731ef7dea69b1a23
Jairo Belen <jairo.belen@codistica.com>
2025-05-06 19:40:42 -0400

    feat: menu updated

--- rabitRoyale/nuevoProyectoDeJuego/Scripts/Menu/character_menu.gd
+++ rabitRoyale/nuevoProyectoDeJuego/Scripts/Menu/character_menu.gd
@@ -12,8 +12,28 @@ func _process(delta: float) -> void:
 	if can_move:
 		motion_ctrl()
 		
+	move_and_slide()
+	
 func motion_ctrl() -> void:
 	'''MOVIMIENTO'''
 	velocity.x = GLOBAL.get_axis().x * SPEED
 	velocity.y -= GRAVITY
 	velocity.z = GLOBAL.get_axis().y * -SPEED
+
+	'''ANIMACIONES'''
+	match is_on_floor():
+		true:
+			if GLOBAL.get_axis() != Vector2.ZERO:
+				$AnimationPlayer.play("Walk")
+			else:
+				$AnimationPlayer.play("Idle")
+	'''ROTATION'''
+	if GLOBAL.get_axis() != Vector2.ZERO:
+		angle = atan2(GLOBAL.get_axis().x , -GLOBAL.get_axis().y)
+		rot = get_rotation()
+		rot.y = angle
+		set_rotation(rot)
+	
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Wave":
+		can_move = true
