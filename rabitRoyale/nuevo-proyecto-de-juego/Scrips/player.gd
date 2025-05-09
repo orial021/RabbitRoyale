c1ae7bf57a2175d639ef84bcec32c70c82d084d1
David <customdesign.4@gmail.com>
2025-05-08 21:14:02 -0400

    feat(player): animationtree finished

--- rabitRoyale/nuevo-proyecto-de-juego/Scrips/player.gd
+++ rabitRoyale/nuevo-proyecto-de-juego/Scrips/player.gd
@@ -1,1 +1,12 @@
 extends CharacterBody3D
+class_name  Player
+
+@onready var GUI: CanvasLayer
+@onready var animation: AnimationTree= $AnimationTree
+@onready var HEAD: Node3D = $Head
+@export var is_dead : bool = false
+@export var id : String
+@export var lives : int = 5
+@export var is_vulnerable : bool = true
+@export var kills : int = 0
+@export var deads : int
