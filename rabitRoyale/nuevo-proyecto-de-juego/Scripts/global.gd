b2f19d83356bb22e61369797dc2591c02f8dd66b
Santiago Garcia <santiagogarciadb@gmail.com>
2025-05-13 20:04:59 -0400

    feat: script de player

--- rabitRoyale/nuevo-proyecto-de-juego/Scripts/global.gd
+++ rabitRoyale/nuevo-proyecto-de-juego/Scripts/global.gd
@@ -12,3 +12,19 @@ func get_axis() -> Vector2:
 	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	axis.y = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_pressed("ui_down"))
 	return axis.normalized()
+	
+const HOST = "http://localhost:8000"
+var token : String
+var headers = PackedStringArray()
+var id : String
+var username : String
+var is_active : bool
+var is_premium : bool
+var coins : int
+var kills : int
+var deads : int
+var wins : int
+var matches_played : int
+var match_id : int
+var get_ready : bool = false
+var is_online : bool = true
--- rabitRoyale/nuevo-proyecto-de-juego/Scripts/player.gd
+++ rabitRoyale/nuevo-proyecto-de-juego/Scripts/player.gd
@@ -12,11 +12,79 @@ class_name Player
 @export var deads : int = 0
 @export var username : String
 var gravity : float = 2.0
-var can_move : bool = false
+var can_move : bool = true
 var can_shot : bool = true
 var is_respawning : bool = false
 var using_scope : bool = false
+var mouse_sensitivity : float = 0.05
 
 const SPEED = 300
 const SCOPING_SPEED = 120
 const JUMP_FORCE = 40
+
+func _enter_tree() -> void:
+	Global.is_online = true
+	
+func _ready() -> void:
+	id = Global.id
+	username = Global.username
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	HEAD.set_rotation_degrees(Vector3.ZERO)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE and event.is_action_pressed("ui_shot"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if not is_dead:
+		if is_on_floor() and event.is_action_pressed("ui_select"):
+			velocity.y = JUMP_FORCE 
+		if event.is_action_pressed("ui_shot") and can_shot:
+			can_shot = false
+			shot_ctrl()
+		if event.is_action_pressed("ui_scope"):
+			using_scope = true
+		if event.is_action_released("ui_scope"):
+			using_scope = false
+
+func shot_ctrl() -> void:
+	pass
+ 
+func _physics_process(delta: float) -> void:
+	velocity.y -= gravity
+	if can_move:
+		HEAD.rotation.x = clamp(HEAD.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+		if not is_dead:
+			$Head/Label3D.text = username + ":" + str(lives)
+			anim_ctrl()
+			motion_ctrl(delta)
+			if velocity.y < -500:
+				lives = 1
+	if is_dead:
+		if not is_respawning:
+			is_respawning = true
+			$Settings/TimerRespawn.start()
+	move_and_slide()
+
+func anim_ctrl() -> void:
+	pass
+
+func motion_ctrl(delta) -> void:
+	var direction = Global.get_axis().rotated(rotation.y)
+	direction = Vector3(direction.x, 0, direction.y)
+ 
+	if is_on_floor():
+		if not using_scope:
+			velocity.x = direction.x * -SPEED * delta
+			velocity.z = direction.z * SPEED * delta
+		else:
+			velocity.x = direction.x * -SCOPING_SPEED * delta
+			velocity.z = direction.z * SCOPING_SPEED * delta
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		HEAD.rotation_degrees.x -= event.relative.y * mouse_sensitivity
+		if not is_dead:
+			rotation_degrees.y -= event.relative.x * mouse_sensitivity
+		else:
+			HEAD.rotation_degrees.y -= event.relative.x * mouse_sensitivity
