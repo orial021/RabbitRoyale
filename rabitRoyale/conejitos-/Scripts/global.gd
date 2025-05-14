43298f4fb229eafc8ae2d526b821849d0a849a6a
hayli <haylimartinez2022@gmail.com>
2025-05-13 19:04:32 -0500

    feat(player):script de player

--- rabitRoyale/conejitos-/Scripts/global.gd
+++ rabitRoyale/conejitos-/Scripts/global.gd
@@ -7,8 +7,25 @@ var can_use_shield : bool = true
 var lives : int = 5 
 var time : int = -1
 var sheep_dead : bool = false 
+var is_online: bool = false
+var username: String
 
 func get_axis() -> Vector2:
 	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	axis.y = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_pressed("ui_down"))
 	return axis.normalized()
+	
+	const HOST = "http://localhost:8000/"
+
+var token : String
+var headers = PackedStringArray()
+var id : String
+var is_active : bool
+var is_premium : bool
+var coins : int
+var kills : int
+var deads : int
+var wins : int
+var matches_played : int
+var match_id : int
+var get_ready : bool = false
